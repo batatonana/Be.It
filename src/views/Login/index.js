5633f4c8eca3acc5f415ef03d5819a7b9cb14554
NazZkKa <guisobreiroalmeida@gmail.com>
2022-11-26 19:52:55 +0000

    Initial Login styling

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,16 +1,21 @@
-
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Pressable, TextInput } from "react-native";
 import { styles } from "./styles";
 import { StatusBar } from "expo-status-bar";
 
-export const Login = ({navigation}) => {
+export const Login = ({ navigation }) => {
   return (
     <View style={styles.container1}>
       <View style={styles.container2}>
-      <Button
-            title="LOGIN"
-            onPress={() => navigation.navigate('Main')}
-        />
+      <Text style={styles.placeholder}>PLACEHOLDER</Text>
+        <TextInput placeholder="Username" style={styles.inputBox}></TextInput>
+        <TextInput placeholder="Password" style={styles.inputBox} secureTextEntry={true}></TextInput>
+        <Pressable
+          onPress={() => navigation.navigate("Main")}
+          style={styles.buttonLogin}
+        >
+          <Text style = {styles.buttonLoginText}>LOGIN</Text>
+        </Pressable>
+        <Text style={styles.signupText}>No account? Sign Up</Text>
       </View>
     </View>
   );
--- src/views/Login/styles.js
+++ src/views/Login/styles.js
@@ -1,25 +1,55 @@
 import { StyleSheet } from "react-native";
 
-
 export const styles = StyleSheet.create({
-    container1: {
-        backgroundColor: '#275309',
-        width:'100%',
-        height:'100%',
-        display: "flex",
-        flex: 1,
-        alignContent: "center",
-        justifyContent: "center",
-    },
-    container2: {
-        flex: 2,
-        backgroundColor: '#E9E6D4',
-        marginHorizontal:'10%',
-        marginBottom:'25%',
-        marginTop:'30%',
-        borderRadius:'25%',
-    },
-    buttonLogin:{
-        
-    }
+  container1: {
+    backgroundColor: "#275309",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container2: {
+    backgroundColor: "#E9E6D4",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "25%",
+    height: "60%",
+    width: "80%",
+  },
+  placeholder: {
+    backgroundColor: "#275309",
+    color: "white",
+    width: 100,
+    height: 50,
+    marginBottom: "30%",
+  },
+  inputBox: {
+    backgroundColor: "white",
+    justifyContent: "center",
+    width: 250,
+    height: 30,
+    margin: "5%",
+    borderColor: "#894303",
+    borderWidth: "1px",
+    paddingLeft: "2.5%",
+    borderRadius: "5%",
+  },
+  buttonLogin: {
+    backgroundColor: "#894303",
+    width: 80,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10%",
+    marginTop: "30%",
+  },
+  buttonLoginText: {
+    color: "white",
+  },
+  signupText: {
+    marginTop: "5%",
+    color: "#275309",
+    textDecorationLine: "underline",
+  },
 });
