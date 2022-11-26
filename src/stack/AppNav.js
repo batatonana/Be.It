224bdfdfd41491e66b9b56438eb8ea5399edbc66
Frederico Ferreira <frederico.ferreira@grama.io>
2022-11-26 15:27:47 +0000

    page for Gui to eddit

--- src/stack/AppNav.js
+++ src/stack/AppNav.js
@@ -1,16 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
 import { Login } from "../views/Auth/Login";
 
 export const AppNav = () => {
-
-    const Stack = createStackNavigator();
-
+    
     return(
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name = 'Main' component={Login}/>
-            </Stack.Navigator>
+            <Login/>
         </NavigationContainer>
     );
 };
--- src/views/Auth/Login/index.js
+++ src/views/Auth/Login/index.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native"
 
 export const Login = () => {
-    <View>
-       <Text>HELLO</Text>
-       <Text style={{color:'#fff'}}>Create</Text>
-       <Text style={{color:'#fff'}}>Create</Text>
-    </View>
+    return(
+        <View>
+        <Text>HELLO</Text>
+     </View>
+    )
+   
 }
