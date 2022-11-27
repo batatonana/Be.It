77562380b83fe9923d77e0cbcbcd05ce8fe66b3d
Frederico Ferreira <frederico.ferreira@grama.io>
2022-11-27 02:52:07 +0000

    comit

--- src/views/Main/Home/index.js
+++ src/views/Main/Home/index.js
@@ -1,18 +1,46 @@
 import { View, Text } from "react-native";
 import { styles } from "./styles";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export const Home = () => {
-    return(
-        <View style={styles.center}>
-            <View>
-                <Text></Text>
-            </View>
-           <View>
+  const variable = "50.00";
 
-           </View>
-           <View>
-            
-           </View>
+  return (
+    <View style={styles.center}>
+      <View style={styles.moneyCard}>
+        <Text style={styles.moneyText}>{variable}$</Text>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.container2}>
+            <Text style={styles.dailyText}>Daily Challenge</Text>
+            <Icon name="timer-outline" size={30} color={"#000000"}/>
         </View>
-    );
+        <View style={styles.bar}/>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.card1}>
+          <Text style={styles.text}>Walk a mile</Text>
+          <Text style={styles.completed}>Completed</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.card1}>
+          <Text style={styles.text}>Take your reusable bottle</Text>
+          <Text style={styles.inProgress}>In progress</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.card1}>
+          <Text style={styles.text}>Quizz</Text>
+          <Text style={styles.inProgress}>In progress</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.card1}>
+          <Text style={styles.text}>Desafio Supresa</Text>
+          <Text style={styles.inProgress}>In progress</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
--- src/views/Main/Home/styles.js
+++ src/views/Main/Home/styles.js
@@ -1,10 +1,76 @@
-import React from "react";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
         center:{
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center", 
             flex:1,
+            backgroundColor:"#ffffff"
+        },
+        moneyCard:{
+            display:"flex",
             alignItems:"center",
-            justifyContent:"center"
+            justifyContent:"center",
+            borderRadius:16,
+            backgroundColor: "#01593D20",
+            width: "85%",
+            height: "13%",
+        },
+        moneyText:{
+            fontSize:"55%",
+        },
+        container:{
+            margin:10,
+            height:"5%",
+            width:"85%",
+            marginBottom: 20,
+        },card:{
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            borderRadius:16,
+            backgroundColor: "#0000000F",
+            width: "85%",
+            height: "10%",
+            marginBottom:"5%",
+        },
+        completed:{
+            color: "#60A05B",
+            fontWeight: "700",
+        },
+        inProgress:{
+            color: "#4A448D",
+            fontWeight: "700",
+        },
+        text:{
+            fontWeight: "600",
+        },
+        card1:{
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            borderRadius:16,
+            backgroundColor: "#ffffff",
+            width: "99%",
+            height:"96%"
+        },
+        container2:{
+            display:"flex",
+            flexDirection:"row",
+            alignItems:"center",
+            justifyContent:"center",
+        },
+        dailyText:{
+            marginRight:"30%",
+            fontWeight:"600",
+            fontSize:20,
+        },
+        bar:{
+            marginTop: 5,
+            borderRadius:"100%",
+            width:"100%",
+            height:8,
+            backgroundColor:"#60A05B",
         }
     });
